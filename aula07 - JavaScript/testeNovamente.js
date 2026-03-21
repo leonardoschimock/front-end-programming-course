7c1a392cef00e9f3658d3bf06559550c2754f03a
Leonardo Schimock <dev.leonardosch@hotmail.com>
2026-03-21 02:24:00 -0300

    Create: typeTest.js

--- aula07 - JavaScript/testeNovamente.js
+++
@@ -1,7 +0,0 @@
-let nome = "Leonardo";
-console.log(typeof(nome));
-
-nome = 1537.98
-console.log(typeof(nome));
-
-console.log(10 == "10"); //true -> fracamente typado.
---
+++ aula07 - JavaScript/typeTest.js
@@ -0,0 +1,9 @@
+// JavaScript é uma linguagem fracamente typada mas de alto nível (linguagem próxima ao vocabulário verbal)
+
+let nome = "Leonardo";
+console.log(typeof(nome));
+
+nome = 1537.98
+console.log(typeof(nome));
+
+console.log(10 == "10"); //true -> não recomendável
